39bc90b706759f3ed3ccd9545e62f9a2596b6a1c
YellowJason <jason90814@gmail.com>
2022-02-24 13:31:32 +0800

    update rand_gen

--- lab1/src/Top.sv
+++ lab1/src/Top.sv
@@ -20,6 +20,10 @@ logic [15:0] counter, counter_nxt;
 
 // 26 bits counter for runing time
 logic [25:0] counter_run, counter_run_nxt;
+parameter step_1 = 26'b00111111111111111111111111;
+parameter step_2 = 26'b01111111111111111111111111;
+parameter step_3 = 26'b10111111111111111111111111;
+parameter counter_end = 26'b11111111111111111111111111;
 
 // random number generator
 logic [15:0] random_num_gen, random_num_gen_nxt;
@@ -51,49 +55,42 @@ always_comb begin
 			// 用一個 counter 計算取隨機數的時間
 			counter_run_nxt = counter_run + 1;
 			// 跑 1.2 秒後回到 IDLE
-			if (counter_run == 26'b11111111111111111111111111) begin
+			if (counter_run == counter_end) begin
 				state_nxt = S_IDLE;
 			end
 			else begin
 				state_nxt = state;
 			end
 			// Linear feedback shift register
-			random_num_gen_nxt[15] = random_num_gen[0] ^ random_num_gen[2] ^ random_num_gen[3] ^ random_num_gen[5];
+			// 將 1.2 秒分成 4 部分
+			random_num_gen_nxt[15] = (random_num_gen[0] ^ random_num_gen[2]) ^ (random_num_gen[3] ^ random_num_gen[5]);
 			random_num_gen_nxt[14:0] = random_num_gen[15:1];
-			if(counter_run <= 8'd13427772) begin
-				if(counter_run % 7'd2685554 == 0) begin
+			if (counter_run <= step_1) begin
+				if (counter_run % 26'b00000011111111111111111111 == 0) begin
 					out_nxt = random_num_gen[3:0];
 				end
 				else
 					out_nxt = out;
 				end
 			end
-			if(counter_run <= 8'd26855544) begin
-				if(counter_run % 7'd6713886 == 0) begin
+			else if (counter_run <= step_2) begin
+				if (counter_run % 26'b00000111111111111111111111 == 0) begin
 					out_nxt = random_num_gen[3:0];
 				end
 				else
 					out_nxt = out;
 				end
 			end	
-			if(counter_run <= 8'd40283316) begin
-				if(counter_run % 8'd13427772 == 0) begin
+			else if (counter_run <= step_3) begin
+				if (counter_run % 26'b00001111111111111111111111 == 0) begin
 					out_nxt = random_num_gen[3:0];
 				end
 				else
 					out_nxt = out;
 				end
 			end	
-			if(counter_run <= 8'd53711088) begin
-				if(counter_run % 8'd26855544 == 0) begin
-					out_nxt = random_num_gen[3:0];
-				end
-				else
-					out_nxt = out;
-				end
-			end	
-			if(counter_run <= 8'd67138860) begin
-				if(counter_run % 8'd67138860 == 0) begin
+			if (counter_run <= counter_end) begin
+				if (counter_run % 26'b00011111111111111111111111 == 0) begin
 					out_nxt = random_num_gen[3:0];
 				end
 				else
@@ -114,6 +111,7 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin	//flipflop
 		counter_run  <= 26'b0;
 		random_num_gen <=  16'b0;
 	end
+	// clock edge
 	else begin
 		out <= out_nxt;
 		state        <= state_nxt;
