32e8103703c500c5ff5b06cdf2ac8ac3fb4d0b2e
Lucas-Lu-coder <lu2001524@gmail.com>
2022-03-01 15:02:23 +0800

    fpga programmable

--- lab1/src/Top - 複製.sv
+++ lab1/src/Top - 複製.sv
@@ -1,7 +1,9 @@
 module Top (
-	input        i_clk,
-	input        i_rst_n,
-	input        i_start,
+	input i_clk,
+	input i_rst_n,	// key1 reset
+	input i_start,	// key0 start
+	input key2,		// key2 pause
+	input key3,		// key3 previous output
 	output [3:0] o_random_out
 );
 
@@ -11,9 +13,10 @@ parameter S_PROC = 1'b1;
 
 // ===== Output Buffers =====
 logic [3:0] out, out_nxt;
-
+logic [3:0] previous, previous_nxt;
+logic [3:0] out_memory, out_memory_nxt;
 // signal when update output
-logic update;
+logic update, u1, u2, u3, u4;
 
 // ===== Registers & Wires =====
 logic state, state_nxt;
@@ -22,11 +25,8 @@ logic state, state_nxt;
 logic [15:0] counter, counter_nxt;
 
 // 26 bits counter for runing time
-logic [25:0] counter_run, counter_run_nxt;
-parameter step_1 = 26'b00111111111111111111111111;
-parameter step_2 = 26'b01111111111111111111111111;
-parameter step_3 = 26'b10111111111111111111111111;
-parameter counter_end = 26'b11111111111111111111111111;
+logic [14:0] counter_run, counter_run_nxt;
+parameter counter_end = 15'b111111111111111;
 
 // random number generator
 logic [15:0] random_num_gen, random_num_gen_nxt;
@@ -36,62 +36,92 @@ assign o_random_out = out;
 
 // ===== Combinational Circuits =====
 always_comb begin
-	counter_nxt = counter + 1;
+	counter_nxt = counter + 1'b1;
+	// 4 段變速
+	u1 = (counter_run[14:13] == 2'b00) & (counter_run[8:0] == 9'b111111111);
+	u2 = (counter_run[14:13] == 2'b01) & (counter_run[9:0] == 10'b1111111111);
+	u3 = (counter_run[14:13] == 2'b10) & (counter_run[10:0] == 11'b11111111111);
+	u4 = (counter_run[14:13] == 2'b11) & (counter_run[11:0] == 12'b111111111111);
 	// FSM
 	case(state)
 		S_IDLE: begin
+			out_memory_nxt = out_memory;
+			counter_run_nxt = 15'b0;
+			update = 1'b0;
 			// 按下 start 按鈕
 			if (i_start) begin
 				state_nxt = S_PROC;
-				out_nxt = 4'b0;
-				counter_run_nxt = 26'b0;
+				out_nxt = out;
 				random_num_gen_nxt = random_num_gen ^ counter;
+				// 記錄前一次答案
+				previous_nxt = out_memory;
 			end
 			else begin
 				state_nxt = state;
-				out_nxt = out;
-				counter_run_nxt = 26'b0;
+				// 按下 key3 顯示上一個答案
+				if (key3 == 1'b0) begin
+					out_nxt = previous;
+				end
+				else begin
+					out_nxt = out;
+				end
 				random_num_gen_nxt = random_num_gen;
+				previous_nxt = previous;
 			end
 		end
 		S_PROC: begin
+			previous_nxt = previous;
 			// 用一個 counter 計算取隨機數的時間
 			counter_run_nxt = counter_run + 1;
 			// 跑 1.2 秒後回到 IDLE
-			if (counter_run == counter_end) begin
+			// 按下 key2 直接暫停
+			if ((counter_run == counter_end) | (key2 == 1'b0)) begin
 				state_nxt = S_IDLE;
+				if (key2 == 1'b0) begin
+					out_memory_nxt = update ? random_num_gen[3:0] : out;
+				end
+				else begin
+					out_memory_nxt = random_num_gen[3:0];
+				end
+				/*
+				if (update == 1'b1) begin
+					out_memory_nxt = random_num_gen[3:0];
+				end
+				else begin
+					out_memory_nxt = out;
+				end
+				*/
 			end
 			else begin
 				state_nxt = state;
+				out_memory_nxt = out_memory;
 			end
 			// Linear feedback shift register
 			// 將 1.2 秒分成 4 部分
 			random_num_gen_nxt[15] = (random_num_gen[0] ^ random_num_gen[2]) ^ (random_num_gen[3] ^ random_num_gen[5]);
 			random_num_gen_nxt[14:0] = random_num_gen[15:1];
 			// output 更新訊號
-			update = (counter_run[25:24] == 2'b00)&(counter_run[19:0] == 20'b11111111111111111111) |
-					 (counter_run[25:24] == 2'b01)&(counter_run[20:0] == 21'b111111111111111111111) |
-					 (counter_run[25:24] == 2'b10)&(counter_run[19:0] == 22'b1111111111111111111111) |
-					 (counter_run[25:24] == 2'b11)&(counter_run[20:0] == 23'b11111111111111111111111);
-			
-			if (counter_run <= step_1) begin
-				if (counter_run[19:0] == 20'b11111111111111111111) begin
+			update = u1 | u2 | u3 | u4;
+			out_nxt = update ? random_num_gen[3:0] : out;
+			/*
+			if (counter_run[14:13] == 2'b00) begin
+				if (counter_run[8:0] == 9'b111111111) begin
 					out_nxt = random_num_gen[3:0];
 				end
 				else begin
 					out_nxt = out;
 				end
 			end
-			else if (counter_run <= step_2) begin
-				if (counter_run[20:0] == 21'b111111111111111111111) begin
+			else if (counter_run[14:13] == 2'b01) begin
+				if (counter_run[9:0] == 10'b1111111111) begin
 					out_nxt = random_num_gen[3:0];
 				end
 				else begin
 					out_nxt = out;
 				end
 			end	
-			else if (counter_run <= step_3) begin
-				if (counter_run[21:0] == 22'b1111111111111111111111) begin
+			else if (counter_run[14:13] == 2'b10) begin
+				if (counter_run[10:0] == 11'b11111111111) begin
 					out_nxt = random_num_gen[3:0];
 				end
 				else begin
@@ -99,13 +129,14 @@ always_comb begin
 				end
 			end	
 			else begin
-				if (counter_run[22:0] == 23'b11111111111111111111111) begin
+				if (counter_run[11:0] == 12'b111111111111) begin
 					out_nxt = random_num_gen[3:0];
 				end
 				else begin
 					out_nxt = out;
 				end
 			end
+			*/
 		end
 	endcase
 end
@@ -118,7 +149,9 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin	//flipflop
 		state        <= S_IDLE;
 		counter		 <= 16'b0;
 		counter_run  <= 26'b0;
-		random_num_gen <=  16'b0;
+		random_num_gen <= 16'b0;
+		previous	 <= 4'b0; 
+		out_memory   <= 4'b0;
 	end
 	// clock edge
 	else begin
@@ -126,7 +159,9 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin	//flipflop
 		state        <= state_nxt;
 		counter      <= counter_nxt;
 		counter_run  <= counter_run_nxt;
-		random_num_gen <=  random_num_gen_nxt;        
+		random_num_gen <=  random_num_gen_nxt;
+		previous	 <= previous_nxt;
+		out_memory   <= out_memory_nxt;
 	end
 	
 end
