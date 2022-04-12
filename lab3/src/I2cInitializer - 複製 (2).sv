4535c711d7019c8ded6f74fd9e8b59693e5a3e75
YellowJason <jason90814@gmail.com>
2022-04-12 14:27:56 +0800

    feat: I2C restart after 24'b

--- lab3/src/I2cInitializer - 複製 (2).sv
+++ lab3/src/I2cInitializer - 複製 (2).sv
@@ -11,9 +11,9 @@ module I2cInitializer (
 parameter S_IDLE = 3'b000;
 parameter S_STAR = 3'b001;
 parameter S_PRE1 = 3'b010;
-parameter S_TEMP = 3'b011; // templary finish (after 24'b transmitted)
+parameter S_PRE2 = 3'b011;
 parameter S_TRAN = 3'b100;
-parameter S_WAIT = 3'b101; // wait for ack
+parameter S_WAIT = 3'b101;
 parameter S_FINI = 3'b110;
 
 // 7 states
@@ -46,15 +46,11 @@ logic [7:0] counter_data, counter_data_nxt;
 // count if transmit 8 bits data
 logic [3:0] counter_tran, counter_tran_nxt;
 
-// count 4 cycles for end & restart
-logic [1:0] counter_temp, counter_temp_nxt;
-
 always_comb begin
 	case(state)
 		S_IDLE: begin
 			counter_data_nxt = 8'b0;
 			counter_tran_nxt = 4'b0;
-			counter_temp_nxt = 2'b0;
 			if (i_start) begin
 				state_nxt = S_STAR;
 				sda_nxt = 1'b0;
@@ -72,7 +68,6 @@ always_comb begin
 		S_STAR: begin
 			counter_data_nxt = counter_data;
 			counter_tran_nxt = counter_tran;
-			counter_temp_nxt = 2'b0;
 			state_nxt = S_PRE1;
 			sda_nxt = data[167];
 			scl_nxt = 1'b0;
@@ -82,7 +77,6 @@ always_comb begin
 		S_PRE1: begin
 			counter_data_nxt = counter_data;
 			counter_tran_nxt = counter_tran;
-			counter_temp_nxt = 2'b0;
 			// transmit 8 bits
 			if (counter_tran == 4'd8) begin
 				state_nxt = S_WAIT;
@@ -105,25 +99,25 @@ always_comb begin
 			end
 			fin_nxt = 1'b0;
 		end
-		S_TEMP: begin
+		/*
+		S_PRE2: begin
 			counter_data_nxt = counter_data;
 			counter_tran_nxt = counter_tran;
-			counter_temp_nxt = counter_temp + 1;
-			state_nxt = (counter_temp == 2'b11) ? S_PRE1 : S_TEMP;
-			scl_nxt = !(counter_temp == 2'b11);
-			oen_nxt = 1'b1;
+			if (counter_data == 8'd168) begin
+				state_nxt = S_FINI;
+			end
+			else begin
+				state_nxt = S_TRAN;
+			end
+			sda_nxt = sda;
+			scl_nxt = 1'b1;
+			oen_nxt = oen;
 			fin_nxt = 1'b0;
-			case(counter_temp)
-				2'b00: sda_nxt = 1'b1;
-				2'b01: sda_nxt = 1'b1;
-				2'b10: sda_nxt = 1'b0;
-				2'b11: sda_nxt = 1'b0;
-			endcase
 		end
+		*/
 		S_TRAN: begin
 			counter_data_nxt = counter_data + 1;
 			counter_tran_nxt = counter_tran + 1;
-			counter_temp_nxt = 2'b0;
 			state_nxt = S_PRE1;
 			oen_nxt = 1'b1; 
 			sda_nxt = (counter_data == 167) ? 1'b0 : data[167-counter_data-1];
@@ -133,25 +127,15 @@ always_comb begin
 		S_WAIT: begin
 			counter_data_nxt = counter_data;
 			counter_tran_nxt = 4'b0;
-			counter_temp_nxt = 2'b0;
+			state_nxt = S_PRE1;
+			sda_nxt = sda;
+			scl_nxt = 1'b0;
 			oen_nxt = 1'b1;
 			fin_nxt = 1'b0;
-			// restart after 24'b transmitted
-			if ((counter_data % 7'd24 == 0) & (counter_data != 8'd168)) begin
-				state_nxt = S_TEMP;
-				sda_nxt = 1'b0;
-				scl_nxt = 1'b1;
-			end
-			else begin
-				state_nxt = S_PRE1;
-				sda_nxt = sda;
-				scl_nxt = 1'b0;
-			end
 		end
 		S_FINI: begin
 			counter_data_nxt = 8'b0;
 			counter_tran_nxt = 3'b0;
-			counter_temp_nxt = 2'b0;
 			state_nxt = S_FINI;
 			sda_nxt = 1'b1;
 			scl_nxt = 1'b1;
@@ -161,7 +145,6 @@ always_comb begin
 		default: begin
 			counter_data_nxt = counter_data;
 			counter_tran_nxt = counter_tran;
-			counter_temp_nxt = counter_temp;
 			state_nxt = state;
 			sda_nxt = sda;
 			scl_nxt = scl;
@@ -175,7 +158,6 @@ always_ff @(negedge i_clk or negedge i_rst_n) begin
 	if (!i_rst_n) begin
 		counter_data <= 8'b0;
 		counter_tran <= 3'b0;
-		counter_temp <= 2'b0;
 		state <= S_IDLE;
 		sda <= 1'b1;
 		scl <= 1'b1;
@@ -185,7 +167,6 @@ always_ff @(negedge i_clk or negedge i_rst_n) begin
 	else begin
 		counter_data <= counter_data_nxt;
 		counter_tran <= counter_tran_nxt;
-		counter_temp <= counter_temp_nxt;
 		state <= state_nxt;
 		sda <= sda_nxt;
 		scl <= scl_nxt;
