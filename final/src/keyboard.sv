6696ba23c3984215564f5655db76395ffc1157e3
YellowJason <jason90814@gmail.com>
2022-04-26 13:51:47 +0800

    new: final folder

---
+++ final/src/keyboard.sv
@@ -0,0 +1,13 @@
+module Keyboard(
+    input i_clk,
+    input i_rst_n,
+    // PS2 input signal
+    input i_data,
+    input i_ps2_clk,
+    
+    output [3:0] o_num1,
+    output [3:0] o_num2
+);
+
+
+endmodule
