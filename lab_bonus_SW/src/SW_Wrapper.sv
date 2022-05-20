c835ed1651ec36fe740766b7fef28547b4b5e301
Lucas-Lu-coder <lu2001524@gmail.com>
2022-05-20 14:41:24 +0800

    wrapper : remove redundant reg, rename data reg, fix core input length

--- lab_bonus_SW/src/SW_Wrapper.sv
+++ lab_bonus_SW/src/SW_Wrapper.sv
@@ -37,7 +37,7 @@ localparam S_WAIT_CALCULATE = 3'b010;
 localparam S_QUERY_TX = 3'b011;
 localparam S_SEND_DATA = 3'b100;
 
-logic [255:0] n_r, n_nxt, d_r, d_nxt, enc_r, enc_nxt, dec_r, dec_nxt;
+logic [255:0] seq_ref_r, seq_ref_nxt, seq_read_r, seq_read_nxt, dec_r, dec_nxt;
 logic [2:0] state_r, state_nxt;
 logic [6:0] bytes_counter_r, bytes_counter_nxt;
 logic [4:0] avm_address_r, avm_address_nxt;
@@ -47,7 +47,7 @@ logic rsa_start_r, rsa_start_nxt;
 logic rsa_finished;
 logic [255:0] rsa_dec;
 
-parameter read_d_start = 7'd33;
+parameter read_seq_read_start = 7'd33;
 parameter data_counter_end = 7'd64;
 parameter write_data_end = 7'd31;
 
@@ -57,19 +57,22 @@ assign avm_write = avm_write_r;
 assign avm_writedata = dec_r[247-:8];
 
 // Remember to complete the port connection
-reg [6:0] row, col;
-reg ready, valid;
-reg [9:0] align_s;
+logic [6:0] row, col;
+logic ready, valid;
+logic [9:0] align_s;
+logic [7:0] seq_ref_len, seq_read_len;
+assign seq_ref_len = 8'd128;
+assign seq_read_len = 8'd128;
 SW_core sw_core(
     .clk				(avm_clk),
     .rst				(avm_rst),
 
 	.o_ready			(ready),
     .i_valid			(1'd0),
-    .i_sequence_ref		(256'd0),
-    .i_sequence_read	(256'd0),
-    .i_seq_ref_length	(8'd0),
-    .i_seq_read_length	(8'd0),
+    .i_sequence_ref		(seq_ref_r),
+    .i_sequence_read	(seq_read_r),
+    .i_seq_ref_length	(seq_ref_len),
+    .i_seq_read_length	(seq_read_len),
     
     .i_ready			(1'd0),
     .o_valid			(valid),
@@ -100,9 +103,8 @@ always_comb begin
     // TODO
     case(state_r)
         S_QUERY_RX:begin
-            n_nxt = n_r;
-            d_nxt = d_r;
-            enc_nxt = enc_r;
+            seq_ref_nxt = seq_ref_r;
+            seq_read_nxt = seq_read_r;
             dec_nxt = dec_r;
             rsa_start_nxt = rsa_start_r;
             if((!avm_waitrequest) && avm_readdata[RX_OK_BIT]) begin
@@ -122,9 +124,8 @@ always_comb begin
                 StartRead(STATUS_BASE);
                 // read the last byte of d in cycle 64
                 if(bytes_counter_r == data_counter_end) begin
-                    n_nxt = n_r;
-                    d_nxt = {d_r[247:0], avm_readdata[7:0]};
-                    enc_nxt = enc_r;
+                    seq_ref_nxt = seq_ref_r;
+                    seq_read_nxt = {seq_read_r[247:0], avm_readdata[7:0]};
                     state_nxt = S_WAIT_CALCULATE;
                     bytes_counter_nxt = 0;
                     rsa_start_nxt = 1'b1;
@@ -132,29 +133,26 @@ always_comb begin
                     avm_read_nxt = 0;
                 end
                 // read d in cycles 33~63
-                else if(bytes_counter_r >= read_d_start) begin
+                else if(bytes_counter_r >= read_seq_read_start) begin
                     // n is received, start reading d 
-                    n_nxt = n_r;
-                    d_nxt = {d_r[247:0], avm_readdata[7:0]};
-                    enc_nxt = enc_r;
+                    seq_ref_nxt = seq_ref_r;
+                    seq_read_nxt = {seq_read_r[247:0], avm_readdata[7:0]};
                     state_nxt = S_QUERY_RX;
                     bytes_counter_nxt = bytes_counter_r;
                     rsa_start_nxt = rsa_start_r;
                 end
                 // read n in cycles 1~32
                 else begin
-                    n_nxt = {n_r[247:0], avm_readdata[7:0]};
-                    d_nxt = d_r;
-                    enc_nxt = enc_r;
+                    seq_ref_nxt = {seq_ref_r[247:0], avm_readdata[7:0]};
+                    seq_read_nxt = seq_read_r;
                     state_nxt = S_QUERY_RX;
                     bytes_counter_nxt = bytes_counter_r;
                     rsa_start_nxt = rsa_start_r;
                 end
             end
             else begin
-                n_nxt = n_r;
-                d_nxt = d_r;
-                enc_nxt = enc_r;
+                seq_ref_nxt = seq_ref_r;
+                seq_read_nxt = seq_read_r;
                 state_nxt = state_r;
                 bytes_counter_nxt = bytes_counter_r;
                 rsa_start_nxt = rsa_start_r;
@@ -164,9 +162,8 @@ always_comb begin
             end
         end
         S_WAIT_CALCULATE:begin
-            n_nxt = n_r;
-            d_nxt = d_r;
-            enc_nxt = enc_r;
+            seq_ref_nxt = seq_ref_r;
+            seq_read_nxt = seq_read_r;
             dec_nxt = rsa_dec;
             state_nxt = rsa_finished ? S_QUERY_TX : S_WAIT_CALCULATE;
             bytes_counter_nxt = bytes_counter_r;
@@ -176,9 +173,8 @@ always_comb begin
             avm_write_nxt = avm_write_r;
         end
         S_QUERY_TX:begin
-            n_nxt = n_r;
-            d_nxt = d_r;
-            enc_nxt = enc_r;
+            seq_ref_nxt = seq_ref_r;
+            seq_read_nxt = seq_read_r;
             dec_nxt = dec_r;
             rsa_start_nxt = rsa_start_r;
             if((!avm_waitrequest) && avm_readdata[TX_OK_BIT]) begin
@@ -197,18 +193,16 @@ always_comb begin
                 StartRead(STATUS_BASE);
                 if(bytes_counter_r == write_data_end) begin
                     // all bytes of dec data are transmitted
-                    n_nxt = n_r;
-                    d_nxt = d_r;
-                    enc_nxt = 0;
+                    seq_ref_nxt = seq_ref_r;
+                    seq_read_nxt = seq_read_r;
                     dec_nxt = dec_r;
                     state_nxt = S_QUERY_RX;
                     bytes_counter_nxt = 64;
                     rsa_start_nxt = 0;
                 end
                 else begin
-                    n_nxt = n_r;
-                    d_nxt = d_r;
-                    enc_nxt = enc_r;
+                    seq_ref_nxt = seq_ref_r;
+                    seq_read_nxt = seq_read_r;
                     dec_nxt = {dec_r[247:0], dec_r[255:248]};
                     state_nxt = S_QUERY_TX;
                     bytes_counter_nxt = bytes_counter_r;
@@ -216,9 +210,8 @@ always_comb begin
                 end
             end
             else begin
-                n_nxt = n_r;
-                d_nxt = d_r;
-                enc_nxt = enc_r;
+                seq_ref_nxt = seq_ref_r;
+                seq_read_nxt = seq_read_r;
                 dec_nxt = dec_r;
                 state_nxt = state_r;
                 bytes_counter_nxt = bytes_counter_r;
@@ -229,9 +222,8 @@ always_comb begin
             end
         end
         default:begin
-            n_nxt = n_r;
-            d_nxt = d_r;
-            enc_nxt = enc_r;
+            seq_ref_nxt = seq_ref_r;
+            seq_read_nxt = seq_read_r;
             dec_nxt = dec_r;
             state_nxt = state_r;
             bytes_counter_nxt = bytes_counter_r;
@@ -246,9 +238,8 @@ end
 // TODO
 always_ff @(posedge avm_clk or posedge avm_rst) begin
     if (avm_rst) begin
-    	n_r <= 0;
-        d_r <= 0;
-        enc_r <= 0;
+    	seq_ref_r <= 0;
+        seq_read_r <= 0;
         dec_r <= 0;
         state_r <= S_QUERY_RX;
         bytes_counter_r <= 0;
@@ -259,9 +250,8 @@ always_ff @(posedge avm_clk or posedge avm_rst) begin
 
     end
 	else begin
-    	n_r <= n_nxt;
-        d_r <= d_nxt;
-        enc_r <= enc_nxt;
+    	seq_ref_r <= seq_ref_nxt;
+        seq_read_r <= seq_read_nxt;
         dec_r <= dec_nxt;
         state_r <= state_nxt;
         bytes_counter_r <= bytes_counter_nxt;
